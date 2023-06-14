0360afe3847ffd60056187690faa23acf13182d0
Patricia Piña <spatricianayeli@gmail.com>
2023-06-13 18:02:14 -0600

    Este es un apunte de operadores lógicos en Java

---
+++ src/operadoresLogicos/ejercicioOperadores.java
@@ -0,0 +1,22 @@
+package operadoresLogicos;
+
+public class ejercicioOperadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* Los operadores lógicos se utilizan para combinar valores booleanos y nos devuelven un resultado verdadero
+		 * && and (y)
+		 * || or (o)
+		 * ! not (no)
+		 */
+		
+		int a = 5;
+		int b = 3;
+		System.out.println(a == b && a > b);
+		System.out.println(a == b || a > b);
+		
+
+	}
+
+}
